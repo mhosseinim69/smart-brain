491f714f44dc4f9cda1ad79fe3430820dfbee7ab
Mostafa Hosseini <m.hosseini.m69@gmail.com>
2023-02-22 15:22:25 +0330

    add url

--- src/App.js
+++ src/App.js
@@ -187,7 +187,7 @@ class App extends Component {
 
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input});
-            fetch('http://localhost:3000/imageurl', {
+            fetch('https://smart-brain-api-rvlc.onrender.com/imageurl', {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
@@ -197,7 +197,7 @@ class App extends Component {
             .then(response => response.json())
             .then(response => {
                 if (response) {
-                    fetch('http://localhost:3000/image', {
+                    fetch('https://smart-brain-api-rvlc.onrender.com/image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
